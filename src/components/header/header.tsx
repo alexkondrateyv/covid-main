baccc3c36a6176f361fc04b8c5032273328d2f0e
Alexander Kondratyev <sashakondrateyv@gmail.com>
2022-09-14 16:09:51 +0300

    feature/header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,23 @@
+import './header.scss';
+
+import logo from '../../images/logo.png';
+import search from '../../images/search.svg';
+
+export function Header() {
+  return (
+    <header className='header'>
+      <div className="header__logo">
+        <img src={logo} alt="logo" className='header__logo-img'/>
+        <h1 className='header__logo-head'>Statistic</h1>
+      </div>
+      <form className="header__search">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="header__search-input"
+        />
+        <img src={search} alt="search" className="header__search-img"/>
+      </form>
+    </header>
+  )
+}
